61bedfd07b767ebb723d2f085f0ff678ef8fd839
Elue-dev <eluewisdom@gmail.com>
2021-10-07 23:02:08 +0100

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../components/style.css'
+
+function Card(props) {
+    let cardData = props.data;
+    const cardList = cardData.map((card, i) => {
+        return(
+            <div key = {i} className = 'container'>
+                <div className = 'box'>
+                    <div className='image'>
+                        <img src = {card.image} />
+                    </div>
+                    <div className = 'text'>
+                        <p>{card.text}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+    return (
+        <div className ='flex'>
+            {cardList}
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        image: "life6.jfif",
+        text: "random text 1"
+    },
+    {
+        image: "life2.jfif",
+        text: "random text 2"
+    },
+    {
+        image: "life3.jfif",
+        text: "random text 3"
+    },
+    {
+        image: "life4.jfif",
+        text: "random text 4"
+    },
+    {
+        image: "life5.jfif",
+        text: "random text 5"
+    }
+]
+
+export default data;
